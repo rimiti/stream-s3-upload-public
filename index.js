ff474f3cd72535dfd1d34763393ba651048bb6eb
Dimitri DO BAIRRO <dimitri.dobairro@dimsolution.com>
2018-11-20 11:42:56 +0100

    feat(index.js) file created

---
+++ index.js
@@ -0,0 +1,27 @@
+const AWS = require('aws-sdk');
+const fs  = require('fs');
+
+AWS.config.update({ accessKeyId: 'ACCESSKEYID', secretAccessKey: 'SECRETACCESSKEY', region: 'eu-west-3'});
+
+const fileStream = fs.createReadStream('/path/to/your/image.png');
+
+fileStream.on('error', function (err) {
+    if (err) { throw err; }
+});
+
+fileStream.on('open', function () {
+    const s3 = new AWS.S3();
+    s3.putObject({
+        Bucket: 'your-bucket',
+        Key: 'file-renamed.png',
+        ACL: 'public-read',
+        Body: fileStream,
+        Metadata: { 'type': 'png', 'user': 'Dimitri DO BAIRRO' }
+    }, function (err) {
+        if (err) { throw err; }
+    });
+});
+
+
+
+
